d68b1e64a5cb4638b0a21c0cd733db7c89a61a92
batfiowoof <bojidarlambov@abv.bg>
2024-07-13 13:13:38 +0300

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <main className="h-screen m">
+      <Sidebar></Sidebar>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+export default function Sidebar() {
+  return (
+    <aside className="w-1/5 bg-gray-800 text-white p-5">
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+      </ul>
+    </aside>
+  );
+}
